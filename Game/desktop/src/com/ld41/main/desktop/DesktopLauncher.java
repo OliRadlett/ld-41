8e9a63aa33799200fa608c5e9d47a205e26d67dd
Oli Radlett <o.radlett@gmail.com>
2018-04-21 06:06:58 +0100

    Add initial LibGDX project

---
+++ Game/desktop/src/com/ld41/main/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ld41.main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ld41.main.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
